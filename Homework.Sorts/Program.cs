7e4fbfe2c904456504d9905369c64ded8688b7d0
Elen Grigoryan <elen.v.grigoryan@gmail.com>
2024-10-22 15:43:41 +0400

    Add project files.

---
+++ Homework.Sorts/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int[] array1 = { 64, 34, 25, 12, 22, 11, 90 };
+// Insertion Sort
+Homework.Sorts.InsertionSort.InsertionSortRecursive(array1);
+Console.WriteLine("Array after Insertion Sort");
+foreach (var item in array1)
+{
+    Console.Write(item+", ");
+}
+Console.WriteLine("\n");
+
+int[] array2 = { 15, 87, 25, 99, 82, 105, 11, 3, 7 };
+//SelectionSort
+Homework.Sorts.SelectionSort.SelectionSortRecursive(array2);
+Console.WriteLine("Array after Selection Sort");
+foreach (var item in array2)
+{
+    Console.Write(item + ", ");
+}
+
+
---
+++ Homework.Sorts/SelectionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Sorts
+{
+    public static class SelectionSort
+    {
+        public static void SelectionSortRecursive(int[] array)
+        {
+            SelectionSortRecursiveHelper(array, 0, array.Length);
+        }
+
+        // Recursive helper function
+        private static void SelectionSortRecursiveHelper(int[] array, int index, int n)
+        {
+            if (index >= n - 1)
+                return;
+
+            int minIndex = index;
+            for (int j = index + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != index)
+            {
+                int temp = array[minIndex];
+                array[minIndex] = array[index];
+                array[index] = temp;
+            }
+
+            SelectionSortRecursiveHelper(array, index + 1, n);
+        }
+    }
+}
